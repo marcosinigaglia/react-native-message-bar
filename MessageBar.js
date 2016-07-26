d3fe07ff788879dc52e2e5dcb69cc69732a74fda
David Alexandrovich <carlespuyol@gmail.com>
2016-07-26 16:27:37 +0300

    Fixed first message issue

--- MessageBar.js
+++ MessageBar.js
@@ -33,23 +33,12 @@ class MessageBar extends Component {
     this.notifyAlertHiddenCallback = null;
     this.alertShown = false;
     this.timeoutHide = null;
-    this.firstCall = false;
 
     this.state = this.getStateByProps(props);
   }
 
-  componentDidMount() {
-    // Apply the colors of the alert depending on its alertType
-    this._applyAlertStylesheet(this.state.alertType);
-
-    // Override the opposition style position regarding the state position in order to have the alert sticks that position
-    this._changeOffsetByPosition(this.state.position);
-  }
-
   componentWillReceiveProps(nextProps) {
-    if (nextProps != null && nextProps.length > 0) {
-      this.setNewState(nextProps);
-    }
+    this.setNewState(nextProps);
   }
 
   setNewState(state) {
@@ -98,16 +87,18 @@ class MessageBar extends Component {
       durationToHide: props.durationToHide || 350,
 
       /* Offset of the View, useful if you have a navigation bar or if you want the alert be shown below another component instead of the top of the screen */
-      viewTopOffset: props.viewTopOffset || 0,
-      viewBottomOffset: props.viewBottomOffset || 0,
-      viewLeftOffset: props.viewLeftOffset || 0,
-      viewRightOffset: props.viewRightOffset || 0,
+      viewTopOffset: props.viewTopOffset || 0,
+      viewBottomOffset: props.viewBottomOffset || 0,
+      viewLeftOffset: props.viewLeftOffset || 0,
+      viewRightOffset: props.viewRightOffset || 0,
+
 
       /* Inset of the view, useful if you want to apply a padding at your alert content */
-      viewTopInset: props.viewTopInset || 0,
-      viewBottomInset: props.viewBottomInset || 0,
-      viewLeftInset: props.viewLeftInset || 0,
-      viewRightInset: props.viewRightInset || 0,
+      viewTopInset: props.viewTopInset || 0,
+      viewBottomInset: props.viewBottomInset || 0,
+      viewLeftInset: props.viewLeftInset || 0,
+      viewRightInset: props.viewRightInset || 0,
+
 
       /* Number of Lines for Title and Message */
       titleNumberOfLines: (props.titleNumberOfLines == undefined) ? 1 : props.titleNumberOfLines,
@@ -130,13 +121,12 @@ class MessageBar extends Component {
   */
   showMessageBarAlert() {
     // If an alert is already shonw or doesn't have a title or a message, do nothing
-    if (this.alertShown || (this.state.title == null && this.state.message == null)) {
+    if (this.alertShown || (this.state.title == null && this.state.message == null)) {
       return;
     }
 
     // Set the data of the alert in the state
     this.alertShown = true;
-    this.firstCall = true;
 
     // Display the alert by animating it from the top of the screen
     // Auto-Hide it after a delay set in the state
@@ -190,7 +180,7 @@ class MessageBar extends Component {
   }
 
 
-  _hideMessageBarAlertComplete() {
+  _hideMessageBarAlertComplete() {
     // The alert is not shown anymore
     this.alertShown = false;
 
@@ -379,7 +369,6 @@ class MessageBar extends Component {
     // Set the animation transformation depending on the chosen animationType, or depending on the state's position if animationType is not overridden
     this._apllyAnimationTypeTransformation();
 
-    if (this.firstCall){
       return (
         <Animated.View style={{ transform: this.animationTypeTransform, backgroundColor: this.state.backgroundColor, borderColor: this.state.strokeColor, borderBottomWidth: 1, position: 'absolute', top: this.state.viewTopOffset, bottom: this.state.viewBottomOffset, left: this.state.viewLeftOffset, paddingTop: this.state.viewTopInset, paddingBottom: this.state.viewBottomInset, paddingLeft: this.state.viewLeftInset, paddingRight: this.state.viewRightInset }}>
           <TouchableOpacity onPress={()=>{this._alertTapped()}} style={{ width: windowWidth }}>
@@ -400,9 +389,6 @@ class MessageBar extends Component {
           </View>
         </Animated.View>
       );
-    } else {
-      return (<View/>);
-    }
   }
 
   renderImage() {
